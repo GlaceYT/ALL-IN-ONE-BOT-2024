b9d2ac17371558bc76318700dff6d704491074c3
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-07-30 15:26:06 +0530

    Update invite.js

--- commands/basic/invite.js
+++ commands/basic/invite.js
@@ -9,6 +9,8 @@ module.exports = {
         const clientId = interaction.client.user.id; 
         const adminPermissions = 8; 
 
+        const inviteURL = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=${adminPermissions}&integration_type=0&scope=bot`;
+
         const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setAuthor({ 
@@ -16,7 +18,7 @@ module.exports = {
                 iconURL: "https://cdn.discordapp.com/attachments/1246408947708072027/1256597293323256000/invite.png?ex=668158ed&is=6680076d&hm=030c83f567ffdaf0bebb95e50baaec8bb8a8394fa1b7717cc43c3622447f58e3&",
                 url: "https://discord.gg/xQF9f9yUEM"
             })
-            .setDescription(`Click [here](https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=${adminPermissions}) to invite the bot to your server with administrator permissions.`)
+            .setDescription(`Click [here](${inviteURL}) to invite the bot to your server!`)
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
