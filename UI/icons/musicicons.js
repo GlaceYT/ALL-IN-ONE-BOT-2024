92659c4acdc2e13488ce6758e471489167852f71
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-07 16:07:23 +0530

    Update musicicons.js

--- UI/icons/musicicons.js
+++ UI/icons/musicicons.js
@@ -1,7 +1,7 @@
 const musicIcons = {
     footerIcon: 'https://cdn.discordapp.com/emojis/1111181271960399935.gif', 
     correctIcon: 'https://cdn.discordapp.com/emojis/819446784647757834.gif', 
-    playerIcon: 'https://cdn.discordapp.com/attachments/1246408947708072027/1257296057633603606/testy.gif?ex=669c4773&is=669af5f3&hm=2c3e855cd410f07e96d280ee60576dd16aa586b1810357680c156507de456b5a&',
+    playerIcon: 'https://cdn.discordapp.com/emojis/763415718271385610.gif',
     wrongIcon: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236666647000125490/9596-wrong.gif?ex=667e0dd0&is=667cbc50&hm=5e30a7f4ad283075fb7430de42fb7985a13eac81f4323110272875ef007fb786&',
     pauseresumeIcon:'https://cdn.discordapp.com/emojis/836145735254540339.gif',
     playIcon:'https://cdn.discordapp.com/attachments/1230824451990622299/1236664581364125787/music-play.gif?ex=669c5e64&is=669b0ce4&hm=b081d67248271167b5aec2e07a2c9c848e16bfa5ba4bdb2067221b0d259c1b38&',
