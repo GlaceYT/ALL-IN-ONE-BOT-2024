633f2e28b9e07a3ca3a8568bd12f684a28ab5673
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-07 16:23:43 +0530

    Update setautorole.js

--- commands/setups/setautorole.js
+++ commands/setups/setautorole.js
@@ -1,10 +1,11 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 const { autoroleCollection } = require('../../mongodb');
 const cmdIcons = require('../../UI/icons/commandicons');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setautorole')
         .setDescription('Set the auto-role for a server')
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageChannels)
         .addStringOption(option =>
             option.setName('serverid')
                 .setDescription('The ID of the server')
@@ -19,6 +20,12 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         if (interaction.isCommand && interaction.isCommand()) {
+            if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+                const embed = new EmbedBuilder()
+                    .setColor('#ff0000')
+                    .setDescription('You do not have permission to use this command.');
+                return interaction.reply({ embeds: [embed], ephemeral: true });
+            }
         const serverId = interaction.options.getString('serverid');
         const roleId = interaction.options.getString('roleid');
         const status = interaction.options.getBoolean('status');
