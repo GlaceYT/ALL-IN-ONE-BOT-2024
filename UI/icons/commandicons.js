665753d8e79d469542dcaa224d150f1e3a47c4b4
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-01-18 00:06:23 +0530

    Delete UI directory

--- UI/icons/commandicons.js
+++
@@ -1,6 +0,0 @@
-const cmdIcons = {
-    dotIcon: 'https://cdn.discordapp.com/emojis/915658913850998794.gif',
-    serverinfoIcon : 'https://cdn.discordapp.com/emojis/942629018296025128.gif',
-};
-
-module.exports = cmdIcons;
